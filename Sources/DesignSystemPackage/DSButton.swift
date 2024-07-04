dc17b6d3782eebcc5d4cb09443d09d6e164d2af9
Vlad Ispas <vlad.ispas@yahoo.com>
2024-07-04 21:17:40 +0300

    Updated DSButton.

--- Sources/DesignSystemPackage/DSButton.swift
+++ Sources/DesignSystemPackage/DSButton.swift
@@ -10,14 +10,20 @@ import SwiftUI
 public struct DSButton: View {
     
     private let title: String
+    private let titleColor: Color
+    private let backgroundColor: Color
     private let action: () -> Void
     
-    /// Both parameters are required for this button.
+    /// The title and action parameters are required for this view. The titleColor and backgroundColor are not required, having the default values set as .black and .gray.opacity(0.1) respectively.
     /// - Parameters:
     ///   - title:  The title of the button
+    ///   - titleColor: The color of the title
+    ///   - backgroundColor: The color of the button's background
     ///   - action: The action that will trigger after the button has been pressed
-    public init(title: String, action: @escaping () -> Void) {
+    public init(title: String, titleColor: Color = .black, backgroundColor: Color = .gray.opacity(0.1), action: @escaping () -> Void) {
         self.title = title
+        self.titleColor = titleColor
+        self.backgroundColor = backgroundColor
         self.action = action
     }
     
@@ -29,10 +35,10 @@ public struct DSButton: View {
                 .frame(height: 44)
                 .frame(maxWidth: .infinity)
                 .font(.system(size: 16, weight: .semibold))
-                .foregroundStyle(.black)
+                .foregroundStyle(titleColor)
                 .background {
                     RoundedRectangle(cornerRadius: 12)
-                        .fill(Color(.gray.opacity(0.1)))
+                        .fill(backgroundColor)
                         .stroke(.black.opacity(0.1), lineWidth: 1)
                 }
         }
